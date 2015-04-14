176d40539cb755b570ae324cab4c2baa395e84bb
Tom Hirashima <tom@straightnochaser.org>
2015-04-14 15:17:39 -0700

    Might as well wrap this all up in a module.

--- main.js
+++ main.js
@@ -1,78 +1,91 @@
 if(!window.TTT) var TTT = {};
 
-TTT.current_player = 'x'; //default starting player
+TTT.game = (function(){
+    var current_player = 'x',
+        form = $(document.forms['board']);
 
-TTT.tileClick = function(){
-    $(this).find('span').addClass('player-'+TTT.current_player);
-    $(this).find('select').val(TTT.current_player).trigger('change');
-};
-TTT.sendMove = function(){
-    var board = {};
-    $(document.forms['board']).find('select').each(function(i){
-        board[this.name] = this.value;
-    });
-    $.post('/move', {'board':board, 'player':TTT.current_player}, function(json){
-        if(json && json.success === true){
+    function tileClick(){
+        $(this).find('span').addClass('player-' + current_player);
+        $(this).find('select').val(current_player).trigger('change');
+    }
 
-            if(json.has_winner === false && json.has_moves === false){
-                TTT.endGame();
-                return;
+    function sendMove(){
+        var board = {};
+        form.find('select').each(function(i){
+            board[this.name] = this.value;
+        });
+        $.post('/move', {'board':board, 'player':current_player}, function(json){
+            if(json && json.success === true){
+
+                if(json.has_winner === false && json.has_moves === false){
+                    endGame();
+                    return;
+                }
+
+                if(json.has_winner === true){
+                    winGame();
+                    return;
+                }
+
+                switchPlayer(json.next_player);
+
             }
+        }, 'JSON');
+    }
 
-            if(json.has_winner === true){
-                TTT.winGame();
-                return;
-            }
+    function showOverlay(msg){
+        var overlay = $('#overlay'),
+            msg_el = $('#overlay .msg'),
+            screen_height = window.innerHeight;
 
-            TTT.switchPlayer(json.next_player);
+        msg_el.text(msg);
+        overlay.height(screen_height+'px');
+        overlay.show();
+    }
 
-        }
-    }, 'JSON');
-};
+    function closeOverlay(){
+        $('#overlay').hide();
+        $('#overlay .msg').text('');
+    }
 
-TTT.showOverlay = function(msg){
-    var overlay = $('#overlay'),
-        msg_el = $('#overlay .msg'),
-        screen_height = window.innerHeight;
+    function endGame(){
+        var msg = "No more moves left!";
+        showOverlay(msg);
+    }
 
-    msg_el.text(msg);
-    overlay.height(screen_height+'px');
-    overlay.show();
-};
+    function winGame(){
+        var msg = 'Player ' + current_player.toUpperCase() + ' has won the game!';
+        showOverlay(msg);
+    }
 
-TTT.closeOverlay = function(){
-    $('#overlay').hide();
-    $('#overlay .msg').text('');
-};
+    function resetGame(){
+        current_player = 'x';
+        form.get(0).reset();
+        form.attr('class', 'player-x');
+        $('#current-player').text('X');
+        form.find('.tile span').removeClass('player-x player-o');
+    }
 
-TTT.endGame = function(){
-    var msg = "No more moves left!";
-    TTT.showOverlay(msg);
-};
+    function switchPlayer(next_player){
+        form.removeClass('player-' + current_player);
+        current_player = next_player;
+        form.addClass('player-' + current_player);
+        $('#current-player').text(current_player.toUpperCase());
+    }
 
-TTT.winGame = function(){
-    var msg = 'Player ' + TTT.current_player.toUpperCase() + ' has won the game!';
-    TTT.showOverlay(msg);
-};
+    return {
+        'sendMove': sendMove,
+        'tileClick': tileClick,
+        'resetGame': resetGame,
+        'closeOverlay': closeOverlay
+    };
 
-TTT.resetGame = function(){
-    document.forms['board'].reset();
-    var form = $(document.forms['board']);
-    form.attr('class', 'player-x');
-    $('#current-player').text('X');
-    form.find('.tile span').removeClass('player-x player-o');
-};
+})();
 
-TTT.switchPlayer = function(next_player){
-    var form = $(document.forms['board']);
-    form.removeClass('player-'+TTT.current_player);
-    TTT.current_player = next_player;
-    form.addClass('player-'+TTT.current_player);
-    $('#current-player').text(TTT.current_player.toUpperCase());
-};
 
+//Attach events
 $(function(){
-    $('select').change(TTT.sendMove);
-    $('.tile').click(TTT.tileClick);
-    $('#overlay button').click(function(){TTT.resetGame(); TTT.closeOverlay();});
+    $('select').change(TTT.game.sendMove);
+    $('.tile').click(TTT.game.tileClick);
+    $('#overlay button').click(function(){TTT.game.resetGame(); TTT.game.closeOverlay();});
 });
